305d4d42336fcd38e19976ee97d332106feb56fd
nella5 <ornella.yazokando@gmail.com>
2021-07-06 15:55:01 +0200

    Creation de la route POSTE

--- server.js
+++ server.js
@@ -1,24 +1,55 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+//Connexion à la bdd
+fastify.register(require('fastify-mongodb'), {
+ 
+  // the default value is false
+  forceClose: true,
+  
+  url: 'mongodb://localhost:27017/superheroes'
+})
+//METHOD API REST
+//GET -READ
+//POST-CREATE
+//PATCH/PUT - UPDATE
+//DELETE -DELETE
 // Declare a route
 fastify.get('/', (request, reply) => {
   return { hello: 'world' }
 })
 
+//Déclarer la route/heroes - Cette route retournera 
+//la liste des avengers
 const avengers=["Iron man", "Captain america", "Spiderman"]
- fastify.get('/heroes',()=>{
+ // heroes GET - obtiens la liste des héros
+fastify.get('/heroes',()=>{
 return{
 	avengers
 }
  })
  
 
+// /heroes POST - Ajoute un nouvel héro
+fastify.post('/heroes', (request, reply) => {
+	console.log(request.body)
+  const collection =fastify.mongo.db.collection("heroes")
+  console.log(collection)
+  collection.insertOne({
+name:request.body.name,
+powerstats:request.body.powerstats,
+
+  })
+	return request.body
+})
+
 fastify.get('/me',function(){
+  console.log("Route/heroes en POST atteinte")
 return{
 	prenom:"Ornella",
-	nom:"lisongo",
+nom:"lisongo",
 	job:"cpw",
+  //toto
 }
 
 })
